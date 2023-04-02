6ee8fb307e69704f836f804308965d0758e2fe63
Pablo <126714702+pablo8203@users.noreply.github.com>
2023-04-02 19:30:42 -0300

    Add files via upload

---
+++ tp6/metodos.java
@@ -0,0 +1,34 @@
+package calc.tp6;
+
+public class metodos{
+
+	public int sumar(int n1, int n2){
+	int resultado = 0;
+	resultado = n1 + n2;
+	return resultado;
+
+	}
+
+	public int restar(int n1, int n2){
+	int resultado = 0;
+	resultado = n1 - n2;
+	return resultado;
+
+	}
+
+	public int multiplicar(int n1, int n2){
+	int resultado = 0;
+	resultado = n1 * n2;
+	return resultado;
+
+	}
+
+	public int dividir(int n1, int n2){
+	int resultado = 0;
+	resultado = n1 / n2;
+	return resultado;
+
+	}
+
+
+}
